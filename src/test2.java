2e9f464378e73164e8ff4bdc6b80985ac0e75cda
david.oberholzer <david.oberholzer@HU226-16.gbsinf2.lokal>
2015-09-14 16:55:26 +0200

    test 2 java

---
+++ src/test2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author david.oberholzer
+ *
+ */
+public class test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
